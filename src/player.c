b91ef516b0a405146b7b8fef6dc455fd794032dd
Shroomy Squid <f.a.r.barrette@hotmail.com>
2024-03-13 11:45:04 -0400

    forgot to mork on branch, start of player line of sight

--- src/player.c
+++ src/player.c
@@ -6,19 +6,79 @@
 /*   By: fbarrett <fbarrett@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 09:12:02 by fbarrett          #+#    #+#             */
-/*   Updated: 2024/03/11 17:39:03 by fbarrett         ###   ########.fr       */
+/*   Updated: 2024/03/13 11:44:12 by fbarrett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cube.h"
+
+void	draw_line(uint32_t middle[2], int angle, t_cube *c)
+{
+	uint32_t x;
+	uint32_t y;
+
+	if (angle == 0)
+	{
+		y = 0;
+		while (y < middle[1])
+		{
+			mlx_put_pixel(c->player, middle[0], y, get_rgba(255, 255, 255, 255));
+			y++;
+		}
+		return ;
+	}
+	if (angle == 90)
+	{
+		x = middle[0];
+		while (x <= c->player->width)
+		{
+			mlx_put_pixel(c->player, x, middle[1], get_rgba(255, 255, 255, 255));
+			x++;
+		}
+		return ;
+	}
+	if (angle == 180)
+	{
+		y = middle[1];
+		while (y < c->player->height)
+		{
+			mlx_put_pixel(c->player, middle[0], y, get_rgba(255, 255, 255, 255));
+			y++;
+		}
+		return ;
+	}
+	if (angle == 270)
+	{
+		x = 0;
+		while (x < middle[0])
+		{
+			mlx_put_pixel(c->player, x, middle[1], get_rgba(255, 255, 255, 255));
+			x++;
+		}
+		return ;
+	}
+}
 
 void	render_player(void *param)
 {
 	t_cube *cube;
 	uint32_t	x;
 	uint32_t	y;
+	int left;
+	uint32_t right;
+	uint32_t middle[2];
 
 	cube = param;
+	left = cube->rotation - 30;
+	if (left < 0)
+		left += 360;
+	right = cube->rotation + 30;
+	if (right > 360)
+		right -= 360;
+	middle[0] = cube->player->width / 2;
+	middle[1] = cube->player->height / 2;
+	draw_line(middle, left, cube);
+	draw_line(middle, right, cube);
 	x = 0;
 	y = 0;
 	while (x < cube->player->width)
@@ -27,9 +87,13 @@ void	render_player(void *param)
 		{
 			if (y > 12 && y < 18 && x > 12 && x < 18)
 				mlx_put_pixel(cube->player, x, y, get_rgba(255, 255, 255, 255));
+			else
+				mlx_put_pixel(cube->player, x, y, get_rgba(255, 255, 255, 0));
 			y++;
 		}
 		y = 0;
 		x++;
 	}
+	draw_line(middle, left, cube);
+	draw_line(middle, right, cube);
 }
--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: lcouturi <lcouturi@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 09:12:02 by lcouturi          #+#    #+#             */
-/*   Updated: 2024/03/06 10:31:55 by fbarrett         ###   ########.fr       */
+/*   Updated: 2024/03/13 10:32:47 by fbarrett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,12 +81,16 @@ static float	*ft_getscale(t_cube *cube, float screenx, int *i)
 
 	screenx -= (float)cube->mlx->width / 2;
 	x = cube->playerx;
+	//printf("py: %f\n", cube->playery);
 	y = cube->playery;
+	//printf("y: %f\n", y);
 	screenx /= cube->mlx->width;
 	while (!touch_wall(cube, x, y))
 		step(&x, &y, cube->rotation + screenx * sqrtf(cube->mlx->width));
 	ret[1] = ft_getside(cube, 2, x, y);
-	ret[0] = (192 / hypotf(fabsf(cube->playery - (int)y), fabsf(cube->playerx - (int)x))) * 384;
+	ret[0] = ((192 / hypotf(fabsf(cube->playery - (int)y), fabsf(cube->playerx - (int)x)))) * 384;
+	//ret[0] -= ((int)ret[0] << 6) >> 6;
+	//printf("%f\n", ret[0]);
 	if (!ret[1])
 		ret[2] = (float)cube->map->walls[(int)ret[1]][i[(int)ret[1]]]->width / 32 * fmodf(x, 32);
 	else if (ret[1] == 1)
