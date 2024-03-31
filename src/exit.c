292148d9c4f7b7edaf103760867a9c038f8bd1b5
Shroomy Squid <f.a.r.barrette@hotmail.com>
2024-03-31 15:45:17 -0400

    changed mlx_terminate location in force_exit

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: fbarrett <fbarrett@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:13:43 by fbarrett          #+#    #+#             */
-/*   Updated: 2024/03/22 16:14:23 by fbarrett         ###   ########.fr       */
+/*   Updated: 2024/03/31 15:42:12 by fbarrett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,11 @@ int	force_exit(t_cube *cube)
 		mlx_delete_texture(cube->lockv_tex);
 	if (cube->pointer_tex)
 		mlx_delete_texture(cube->pointer_tex);
-	mlx_terminate(cube->mlx);
 	if (cube->map && cube->map->walls)
 		free_walls(cube->map->walls);
 	if (cube->map && cube->map->map)
 		free_all(cube->map->map);
+	mlx_terminate(cube->mlx);
 	if (cube->map)
 		free(cube->map);
 	free(cube);
