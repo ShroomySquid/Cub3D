ac571704bb22a92a618f5155333a0ac0fce2a66e
Shroomy Squid <f.a.r.barrette@hotmail.com>
2024-04-02 10:24:59 -0400

    added error/n in error msg to fit subject

--- src/error_msg.c
+++ src/error_msg.c
@@ -6,7 +6,7 @@
 /*   By: fbarrett <fbarrett@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 09:12:02 by fbarrett          #+#    #+#             */
-/*   Updated: 2024/04/02 10:03:00 by fbarrett         ###   ########.fr       */
+/*   Updated: 2024/04/02 10:21:24 by fbarrett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,28 @@
 
 int	error_func(char *failed_func)
 {
+	printf("Error\n");
 	printf("Function %s failed\n", failed_func);
 	return (1);
 }
 
 int	error_argc(void)
 {
+	printf("Error\n");
 	printf("Invalid amount of arguments\n");
 	return (1);
 }
 
 int	error_map(char *str, int x, int y)
 {
+	printf("Error\n");
 	printf("Invalid map: %s at x: %d, y: %d\n", str, x, y);
 	return (1);
 }
 
 int	error_map_file(char *str)
 {
+	printf("Error\n");
 	printf("Invalid map: %s\n", str);
 	return (1);
 }
